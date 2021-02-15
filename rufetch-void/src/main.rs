1744fc22c31d514f82c07b6dc5432d10ade216f7
sec <sec@sec-void.station>
2021-02-15 09:14:06 +0100

    Using constants for colors

--- rufetch-void/src/main.rs
+++ rufetch-void/src/main.rs
@@ -1,6 +1,20 @@
 use std::process::Command;
 use std::env;
 
+
+// __--COLORS--__
+
+const RESET: &str = "\x1B[0m";
+
+// ample/coffee theme
+const C1: &str = "\x1B[0m\x1B[1m\x1B[38;2;192;103;9m"; // orange
+const C2: &str = "\x1B[0m\x1B[1m\x1B[38;2;226;78;78m"; // red
+const FONT1: &str = "\x1B[0m\x1B[1m\x1B[38;2;226;78;78m";
+const FONT2: &str = "\x1B[0m\x1B[1m\x1B[38;2;192;103;9m";
+// ample/coffee theme
+
+// --__COLORS__--
+
 macro_rules! cmd {
     ($cmd:expr) => {
         String::from_utf8(
@@ -34,18 +48,6 @@ fn main() {
     let shell = cmd!("basename", env::var("SHELL").unwrap());
     let wm = env::var("WM").unwrap();
 
-    // __--COLORS--__
-
-    let reset = "\x1B[0m";
-
-    // ample/coffee theme
-    let c1 = "\x1B[0m\x1B[1m\x1B[38;2;192;103;9m"; // orange
-    let c2 = "\x1B[0m\x1B[1m\x1B[38;2;226;78;78m"; // red
-    let font1 = "\x1B[0m\x1B[1m\x1B[38;2;226;78;78m";
-    let font2 = "\x1B[0m\x1B[1m\x1B[38;2;192;103;9m";
-    // ample/coffee theme
-
-    // --__COLORS__--
 
     print!(
 "      {}_______      {}{}{}@{}{}
@@ -57,13 +59,13 @@ fn main() {
    {}-_______\\       {}WM:{}        {}{}
 
 ",
-        c1, font2, user, font1, font2, host,
-        c1, font1, reset, osname,
-        c1, c2, c1, font1, reset, kernel,
-        c1, c2, c1, font1, reset, uptime,
-        c1, c2, c1, font1, reset, packages,
-        c1, font1, reset, shell,
-        c1, font1, reset, wm, reset)
+        C1, FONT2, user, FONT1, FONT2, host,
+        C1, FONT1, RESET, osname,
+        C1, C2, C1, FONT1, RESET, kernel,
+        C1, C2, C1, FONT1, RESET, uptime,
+        C1, C2, C1, FONT1, RESET, packages,
+        C1, FONT1, RESET, shell,
+        C1, FONT1, RESET, wm, RESET)
 
 
 
